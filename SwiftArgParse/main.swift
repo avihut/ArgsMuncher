a7a9794a9eb169c91a16e21509990d08c97e0c3d
avihut <avihu.turzion@gmail.com>
2017-09-10 01:23:25 +0300

    Initial Commit

---
+++ SwiftArgParse/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftArgParse
+//
+//  Created by Avihu Turzion on 10/09/2017.
+//  Copyright © 2017 Avihu Turzion. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
